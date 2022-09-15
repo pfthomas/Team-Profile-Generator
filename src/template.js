ca1b0484ff700bddd2fde6562f3b559a1ebd2cf5
pftho <pf.thomas97@gmail.com>
2022-09-14 22:56:20 -0400

    fixed html

--- src/template.js
+++ src/template.js
@@ -22,7 +22,7 @@ const createTeam = (team) => {
           <h4 class="card-title">${engineer.getRole()}</h4>
           <ul class="list-group">
             <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a>< li>
+            <li class="list-group-item">Email: <a href="mailto: ${engineer.getEmail()}">${engineer.getEmail()}</a><li>
             <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
             </ul>
         </div>
